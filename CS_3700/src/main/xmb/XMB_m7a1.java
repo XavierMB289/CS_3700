69bbaf9a59ffbf12aa9c3dcd1df1657e2f885234
XavierMB289 <silverneon123@gmail.com>
2026-06-06 01:10:04 -0400

    Additional commenting

--- CS_3700/src/main/xmb/XMB_m7a1.java
+++ CS_3700/src/main/xmb/XMB_m7a1.java
@@ -13,12 +13,14 @@ public class XMB_m7a1 {
 		Arrays.fill(students, null);
 		//Actually creating the students
 		for(int i = 0; i < students.length; i++) {
+			//Variable set up
 			String name = names[(int)Math.round(Math.random()*names.length)];
 			int age = (int)(Math.random()*100);
 			String email = name+"_"+i+"@cheesy.edu.net.com.org";
 			String phoneNum = "555-"+(int)(Math.random()*10000);
 			int stage = (int)(Math.random()*10);
 			String[] courses = new String[(int)(Math.random()*6)];
+			//Plug 'n chug
 			for(int j = 0; j < courses.length; j++) {
 				courses[j] = courseStarts[(int)(Math.random()*courseStarts.length)] + "_" + (int)(Math.random()*1000);
 			}
@@ -36,9 +38,9 @@ public class XMB_m7a1 {
 class Student{
 	
 	//Variable Declarations
-	private String name, email, phoneNum;
-	private int age, stage;
-	private String[] courses;
+	private final String name, email, phoneNum;
+	private final int age, stage;
+	private final String[] courses;
 	
 	/**
 	 * Student setup
